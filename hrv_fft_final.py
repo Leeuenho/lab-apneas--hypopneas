b868b092fb32ac285d5858e982af09cd879dc0aa
Leeuenho <80818827+Leeuenho@users.noreply.github.com>
2021-08-17 18:42:27 +0900

    RR interval fft
    (코드 수정)
    bmp이용 -> RR 간격 이용한 data처리
    중복 기능 코드 삭제
    hrv 분석을 위한 stem data 유지

--- hrv_fft_final.py
+++ hrv_fft_final.py
@@ -12,28 +12,28 @@ import warnings
 warnings.filterwarnings('ignore')
 
 fdir = 'D:/한림대학교/연구실/git_lab//'
-fn = '1m_HRV_최종환_PRE_30s.csv'
+fn1 = '1m_HRV_최종환_PRE_30s.csv'
+fn2 = '1m_HRV_최종환_PRE_30s_1.csv'
 
-file = fdir+fn
-data = pd.read_csv(file, encoding = "UTF-8")
+file1 = fdir+fn1
+data1 = pd.read_csv(file1, encoding = "UTF-8")
 
-#HRV 값 가져오기
-hrv = data['HRV'].values
+file2 = fdir + fn2
+data2 = pd.read_csv(file2, encoding = "UTF-8")
 
+#HRV 값 가져오기 (RR interval)
+hrv = data1['HRV'].values
 
-#소수점 2까지 hrv 구하기
-y = [0.0]*75
-for i in range(0, (len(hrv))):
-    y[i] = round((60000/hrv[i]),2)
 
 #x축 설정
 x = [0.0]*75
 
 for i in range(0, (len(hrv))) :
    if (i == 0):
-       x[i] = y[i]
+       x[i] = hrv[i]
    else:
-        x[i] = np.add(x[i-1],y[i])
+        x[i] = np.add(x[i-1], hrv[i])
+
 
 import matplotlib.pyplot as plt
 from scipy.interpolate import interp1d
@@ -41,11 +41,11 @@ import numpy as np
 
 
 #1024개 data로 interpolation
-fl = interp1d(x,y, kind= 'linear')
+fl = interp1d(x, hrv, kind= 'linear')
 xint = np.linspace(min(x), max(x), 1024)
 yint = fl(xint)
 
-plt.plot(x,y)
+plt.plot(x, hrv)
 plt.plot(xint, yint, 'x')
 
 plt.show()
@@ -53,8 +53,13 @@ plt.show()
 
 
 #n과 주파수 선언
+
+time = data2['Time'].values[-1]
+a = time.split(':')
+a = list(map(int, a))
+s = a[0]*3600 + a[1]*60 + a[2]
 n = 1024
-s = 60
+
 fs = n/s
 t = 1/fs
 fd = fs/n
@@ -75,7 +80,13 @@ from scipy.signal import detrend
 
 #dc 주파수인 0Hz 제거
 dc_rm = yint - np.mean(yint)
-dc_rm = detrend(dc_rm)
+
+
+#dc_rm = detrend(dc_rm) 
+# detred
+# signal의 값 (point) 아래에 stem(DC) 값 존재
+# 그것을 제거하는 함수
+# 대부분의 신호 처리에서 사용하나 HRV에서는 거의 제거 안하는 편
 
 #fft
 X = fftpack.fft(dc_rm)
@@ -89,16 +100,32 @@ ax.set_ylabel('Frequency Domain (Spectrum) Magnitude')
 ax.set_xlim(0, fs / 20)
 
 #-----------------------------------------
-#dc 주파수인 0Hz 제거
-dc_rm = yint - np.mean(yint)
-dc_rm = detrend(dc_rm)
+# #dc 주파수인 0Hz 제거
+# dc_rm = yint - np.mean(yint)
+# #dc_rm = detrend(dc_rm)
 
-#fft
-Y = np.fft.fft(dc_rm)/n
-Y = Y[range(int(n/2))]
+# #fft
+# Y = np.fft.fft(dc_rm)/n  >>>>> 자동으로 detred 
+# Y = Y[range(int(n/2))]
 
 
-fig,ax = plt.subplots()
-ax.stem(f, np.abs(Y))
-ax.set_xlim(0, fs/10)
+# fig,ax = plt.subplots()
+# ax.stem(f, np.abs(Y))
+# ax.set_xlim(0, fs/20)
 
+
+lf_sum = 0
+hf_sum = 0
+
+for i in range ( 0, len(freqs)):
+    if freqs[i] >= 0.04 and freqs[i] < 0.14 :
+        lf_sum += np.abs(X[i])
+
+    elif freqs[i] >= 0.14 and freqs[i] < 0.4 :
+        hf_sum += np.abs(X[i])
+        
+    else : 
+        continue
+
+lf = np.log(lf_sum)
+hf = np.log(hf_sum)
