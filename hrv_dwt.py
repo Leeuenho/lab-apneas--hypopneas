700e50b84fed3a881591138672276d45f15d39db
Leeuenho <80818827+Leeuenho@users.noreply.github.com>
2021-09-09 17:53:31 +0900

    HRV_dwt
    코드수정

--- hrv_dwt.py
+++ hrv_dwt.py
@@ -18,6 +18,7 @@ file = fdir+fn
 data = pd.read_csv(file)
 hrv = data['HRV'].values
 
+
 x = [0.0]*len(hrv)
 
 for i in range (0, len(hrv)):
@@ -27,54 +28,142 @@ for i in range (0, len(hrv)):
         x[i] = x[i-1] + hrv[i]
         
         
+from scipy.interpolate import interp1d
+
+
+#1024개 data로 interpolation
+fl = interp1d(x, hrv, kind= 'linear')
+xint = np.linspace(min(x), max(x), 5000)
+yint = fl(xint)
+
         
-# t = x[len(x)-1] / 1000
-# n = len(hrv)
-# fs = n/t
-# f = (np.arange(n)) * fs
-# f = f[range(int(n/2))]
+        
+t = x[len(x)-1] / 1000
+n = len(yint)
+fs = n/t
 
 
-data[0] = hrv
 
 import pywt
 
 import matplotlib.pyplot as plt
     
 
-coeff = pywt.wavedec(hrv, 'db4', level = 18)
+#dwt 최대 레벨 찾기
+max_level = pywt.dwt_max_level(5000, 'db4')
 
-cl18, cl17h, cl16h, cl15h, cl14h, cl13h, cl12h, cl11h, cl10h, cl9h, cl8h, cl7h, cl6h, cl5h, cl4h, cl3h, cl2h, clh, ch = coeff
-
-f = pywt.scale2frequency('db4', 18)
-
-
+#dwt 수행
+coeff = pywt.wavedec(yint, 'db4', level = 9)
 
 
+for i in range(len(coeff)):
+    coeff[i] = abs(coeff[i]**2)
+
+
+l9, l8h, l7h, l6h, l5h, l4h, l3h, l2h, lh, h = coeff
 
 
 
-# 18 = 0.39/ 5 = 0.14/ 2 = 0.35
+f = [0.0]*11
+
+for i in range(0,10):
+    f[i] = 2**(-i)
+
+f1 = np.linspace(f[0], f[1], len(h))
+f2 = np.linspace(f[1], f[2], len(lh))
+f3 = np.linspace(f[2], f[3], len(l2h))
+f4 = np.linspace(f[3], f[4], len(l3h))
+f5 = np.linspace(f[4], f[5], len(l4h))
+f6 = np.linspace(f[5], f[6], len(l5h))
+f7 = np.linspace(f[6], f[7], len(l6h))
+f8 = np.linspace(f[7], f[8], len(l7h))
+f9 = np.linspace(f[8], f[9], len(l8h))
+f10 = np.linspace(f[9], f[10], len(l9))
+
+
+
+
+h = np.c_[h, f1]
+lh = np.c_[lh, f2]
+l2h = np.c_[l2h, f3]
+l3h = np.c_[l3h, f4]
+l4h = np.c_[l4h, f5]
+l5h = np.c_[l5h, f6]
+l6h = np.c_[l6h, f7]
+l7h = np.c_[l7h, f8]
+l8h = np.c_[l8h, f9]
+l9 = np.c_[l9, f10]
+
+
+
+
 
 lf = 0
 hf = 0
 
-for i in range(len(coeff)):
-    abs(coeff[i]**2)
+for i in range(len(h)-1):
+    if(h[i][1] >= 0.04 and h[i][1] <= 0.15):
+        lf += h[i][0]
+    elif(h[i][1] >= 0.15 and h[i][1] <= 0.4):
+        hf += h[i][1]
 
+for i in range(len(lh)):
+    if(lh[i][1] >= 0.04 and lh[i][1] <= 0.15):
+        lf += lh[i][0]
+    elif(lh[i][1] >= 0.15 and lh[i][1] <= 0.4):
+        hf += lh[i][0]
 
-for i in range(len(coeff)):
-    if (i>=5 & i<=17):
-        for k in range(len(coeff[i])):
-            lf += coeff[i][k]
-            
-            
-for i in range(len(coeff)):
-    if (i>=1 & i<=5):
-        for k in range(len(coeff[i])):
-            hf += coeff[i][k]
+for i in range(len(l2h)):
+    if(l2h[i][1] >= 0.04 and l2h[i][1] <= 0.15):
+        lf += l2h[i][0]
+    elif(l2h[i][1] >= 0.15 and l2h[i][1] <= 0.4):
+        hf += l2h[i][0]
+        
+for i in range(len(l3h)):
+    if(l3h[i][1] >= 0.04 and l3h[i][1] <= 0.15):
+        lf += l3h[i][0]
+    elif(l3h[i][1] >= 0.15 and l3h[i][1] <= 0.4):
+        hf += l3h[i][0]
+        
+for i in range(len(l4h)):
+    if(l4h[i][1] >= 0.04 and l4h[i][1] <= 0.15):
+        lf += l4h[i][0]
+    elif(l4h[i][1] >= 0.15 and l4h[i][1] <= 0.4):
+        hf += l4h[i][0]
+        
+for i in range(len(l5h)):
+    if(l5h[i][1] >= 0.04 and l5h[i][1] <= 0.15):
+        lf += l5h[i][0]
+    elif(l5h[i][1] >= 0.15 and l5h[i][1] <= 0.4):
+        hf += l5h[i][0]       
 
+for i in range(len(l6h)):
+    if(l6h[i][1] >= 0.04 and l6h[i][1] <= 0.15):
+        lf += l6h[i][0]
+    elif(l6h[i][1] >= 0.15 and l6h[i][1] <= 0.4):
+        hf += l6h[i][0]
 
+for i in range(len(l7h)):
+    if(l7h[i][1] >= 0.04 and l7h[i][1] <= 0.15):
+        lf += l7h[i][0]
+    elif(l7h[i][1] >= 0.15 and l7h[i][1] <= 0.4):
+        hf += l7h[i][0]
+        
+for i in range(len(l8h)):
+    if(l8h[i][1] >= 0.04 and l8h[i][1] <= 0.15):
+        lf += l8h[i][0]
+    elif(l8h[i][1] >= 0.15 and l8h[i][1] <= 0.4):
+        hf += l8h[i][0]
+
+for i in range(len(l9)):
+    if(l9[i][1] >= 0.04 and l9[i][1] <= 0.15):
+        lf += l9[i][0]
+    elif(l9[i][1] >= 0.15 and l9[i][1] <= 0.4):
+        hf += l9[i][0]
+        
+
+ln_lf = np.log(lf)
+ln_hf = np.log(hf)
     
     
 # plt.subplot(5,1,1)
